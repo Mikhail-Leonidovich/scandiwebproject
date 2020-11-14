cc2e7ec3a61420ee3b8b4bb3ffeadc224843f406
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-11-14 12:55:42 +0300

    added button functionality

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,18 +3,58 @@ import "../styles/Carousel.css";
 import imageForest from "../images/imageForest.jpg";
 import imageOcean from "../images/imageOcean.jpg";
 import imageShips from "../images/imageShips.jpg";
+import imageTiger from "../images/tiger.jpg";
 
 class Carousel extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    let initialValue = 0;
+
+    this.state = {
+      newValue: 0,
+      style: {
+        transform: `translateX(${initialValue}%)`,
+      },
+    };
   }
+
+  handleChangeLeft = () => {
+    if (this.state.newValue != 0) {
+      this.setState((state) => {
+        let { transform } = state.style;
+        let { newValue } = state;
+
+        let newCurrentValue = newValue + 25;
+        transform = `translateX(${newCurrentValue}%)`;
+        return { style: { transform: transform }, newValue: newCurrentValue };
+      });
+    }
+  };
+
+  handleChangeRight = () => {
+    if (this.state.newValue > -75) {
+      this.setState((state) => {
+        let { transform } = state.style;
+        let { newValue } = state;
+
+        if (newValue >= 0) {
+          let newCurrentValue = newValue - 25;
+          transform = `translateX(${newCurrentValue}%)`;
+          return { style: { transform: transform }, newValue: newCurrentValue };
+        } else {
+          let newCurrentValue = -(Math.abs(newValue) + 25);
+          transform = `translateX(${newCurrentValue}%)`;
+          return { style: { transform: transform }, newValue: newCurrentValue };
+        }
+      });
+    }
+  };
 
   render() {
     return (
       <div className="carousel">
-        <div className="slider">
+        <div className="slider" style={this.state.style}>
           <div className="slider__elem">
             <img className="slider__img" src={imageForest} alt="forest"></img>
           </div>
@@ -24,14 +64,17 @@ class Carousel extends Component {
           <div className="slider__elem">
             <img className="slider__img" src={imageShips} alt="ships"></img>
           </div>
+          <div className="slider__elem">
+            <img className="slider__img" src={imageTiger} alt="tiger"></img>
+          </div>
         </div>
 
         <div className="controls">
-          <button className="arrow left">
-            <div className="previos"></div>
+          <button className="arrow previos" onClick={this.handleChangeLeft}>
+            <div className="left"></div>
           </button>
-          <button className="arrow right">
-            <div className="next"></div>
+          <button className="arrow next" onClick={this.handleChangeRight}>
+            <div className="right"></div>
           </button>
         </div>
       </div>
